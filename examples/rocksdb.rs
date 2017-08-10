3f7d0c7e40972ee132a6041039ab40bfe7b0f249
Oleksandr Anyshchenko <oleksandr.anyshchenko@xdev.re>
2017-08-10 15:48:44 +0300

    Renamed pakage name

--- examples/rocksdb.rs
+++ examples/rocksdb.rs
@@ -26,7 +26,8 @@ fn main() {
 
         let mut txn_opts = TransactionOptions::default();
         txn_opts.set_deadlock_detect(true);
-        txn_opts.set_expiration(2);
+        txn_opts.set_snapshot(true);
+        txn_opts.set_expiration(5);
 
         let mut w_opts = WriteOptions::default();
         w_opts.set_sync(true);
@@ -40,7 +41,6 @@ fn main() {
         let _ = db.put(b"key2", b"value2");
 
         let txn = db.transaction_begin(&w_opts, &txn_opts);
-
         let _ = txn.put(b"key3", b"value3");
 
         {
@@ -50,8 +50,12 @@ fn main() {
                          String::from_utf8(key.to_vec()).unwrap(),
                          String::from_utf8(value.to_vec()).unwrap());
             }
+
+            let _ = txn2.put(b"key4", b"value5");
+//            txn2.commit();
         }
 
+        assert!(txn.get(b"key4").unwrap().is_some());
 
         thread::sleep(Duration::from_millis(1));
 
